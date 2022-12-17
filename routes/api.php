9a8d27581df4b446fdb882193194d552c0c2290c
JulianErnest <76834365+JulianErnest@users.noreply.github.com>
2022-12-17 09:38:08 +0800

    [feature] basic authentication

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Private routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+  Route::get('/token', [AuthController::class, 'getUserDetails']);
+  Route::post('/logout', [AuthController::class, 'logout']);
 });
